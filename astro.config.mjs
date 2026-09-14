9ab9bf54776f25b458e7da2ccd00727d7aa6648f
Tejas Badone <tejas.badone1@areno.in>
2026-09-14 16:44:48 +0530

    ready to share

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://cashahidraza.in",
+  site: "https://scottokamotolaw.com",
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
